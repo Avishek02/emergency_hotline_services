27b301e4871287b0b325a318784aa7025e4f3ab0
Avishek02 <avitong9@gmail.com>
2025-08-30 22:07:42 +0600

    fist connection with github

---
+++ script.js
@@ -0,0 +1,127 @@
+const favoriteCounterElement = document.getElementById('favorite-counter');
+const coinCounterElement = document.getElementById('coin-counter');
+const copyCounterElement = document.getElementById('copy-counter');
+const callHistoryContainerElement = document.getElementById('history-list-container');
+const clearButton = document.getElementById('btn-clear');
+const hamburgerButton = document.getElementById('btn-hamburger');
+const mobileMenu = document.getElementById('counter-icons-container');
+
+
+
+const favoriteButtons = document.querySelectorAll('.favorite-icon');
+const callButtons = document.querySelectorAll('button[id $= "-btn-call"]');
+const copyButtons = document.querySelectorAll('button[id $= "-btn-copy"]');
+
+
+let favoriteCount = 0;
+let coinCount = 100;
+let copyCount = 0;
+
+
+function getCurrentTime() {
+    const now = new Date();
+    return now.toLocaleTimeString('en-US', {
+        timeZone: 'Asia/Dhaka',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+}
+
+
+for (const button of favoriteButtons) {
+    button.addEventListener('click', () => {
+        favoriteCount++;
+        favoriteCounterElement.textContent = favoriteCount;
+    });
+}
+
+for (const button of callButtons) {
+    button.addEventListener('click', () => {
+        if (coinCount > 0) {
+            coinCount -= 20;
+            coinCounterElement.textContent = coinCount;
+
+
+            const servicePrefix = button.id.replace('-btn-call', '');
+            const serviceNameElement = document.getElementById(`${servicePrefix}-service-name`);
+            const serviceNumberElement = document.getElementById(`${servicePrefix}-service-number`);
+
+
+            const serviceName = serviceNameElement.textContent.replace(/\s+/g, ' ').trim();
+            const serviceNumber = serviceNumberElement.textContent.replace(/\s+/g, ' ').trim();
+
+            const clickTime = getCurrentTime();
+
+            const callHistoryItemHTML = `
+                        <div class="tertiary-bg-color font-hind-madurai-regular flex justify-between items-center p-4 rounded-lg gap-4">
+
+                            <div class = "space-y-1">
+                                <p >${serviceName}</p>
+                                <p >${serviceNumber}</p>
+                            </div>
+
+                            <p >${clickTime}</p>
+                        </div>
+        `;
+
+
+            callHistoryContainerElement.insertAdjacentHTML('afterbegin', callHistoryItemHTML);
+
+            alert(`Calling ${serviceName} ${serviceNumber}`);
+
+        } else {
+            alert("আপনার পর্যাপ্ত কয়েন নেই। কল করার জন্য কমপক্ষে ২০টি কয়েন লাগবে।");
+        }
+
+
+
+    });
+}
+
+
+
+for (const button of copyButtons) {
+    button.addEventListener('click', () => {
+        copyCount++;
+        copyCounterElement.textContent = copyCount;
+
+
+
+        const servicePrefix = button.id.replace('-btn-copy', '');
+        const serviceNumberElement = document.getElementById(`${servicePrefix}-service-number`);
+        const numberToCopy = serviceNumberElement.textContent.trim();
+
+
+        navigator.clipboard.writeText(numberToCopy).then(() => {
+            alert(`${numberToCopy} নাম্বারটি কপি হয়েছে`);
+        }).catch(err =>{
+            console.error('could not copy text', err);
+        });
+
+    });
+}
+
+
+clearButton.addEventListener('click', () => {
+    callHistoryContainerElement.innerHTML = '';
+});
+
+
+
+
+
+
+
+
+// Mobile responsive 
+const hambugerIcon = document.getElementById('icon-hamburger');
+const closeIcon = document.getElementById('icon-close');
+
+hamburgerButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    hambugerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+});
+
